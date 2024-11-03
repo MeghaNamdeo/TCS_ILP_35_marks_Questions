c726f32a0fcb0057fb6456734f51affb2aec9a27
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-11-03 21:08:27 +0530

    Update 08_Phone.java

--- 08_Phone.java
+++ 08_Phone.java
@@ -65,3 +65,104 @@ Output
 ------------------
 The given Brand is not available
 222
+
+
+  import java.util.Scanner;
+
+class Phone {
+    private int phoneId;
+    private String os;
+    private String brand;
+    private int price;
+
+    public Phone(int phoneId, String os, String brand, int price) {
+        this.phoneId = phoneId;
+        this.os = os;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public int getPhoneId() {
+        return phoneId;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPhoneId(int phoneId) {
+        this.phoneId = phoneId;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Phone[] arr = new Phone[4];
+
+        for (int i = 0; i < 4; i++) {
+            int phoneId = sc.nextInt(); sc.nextLine();
+            String os = sc.nextLine();
+            String brand = sc.nextLine();
+            int price = sc.nextInt(); sc.nextLine();
+            arr[i] = new Phone(phoneId, os, brand, price);
+        }
+
+        String brandToSearch = sc.nextLine();
+        String osToSearch = sc.nextLine();
+
+        int totalPrice = findPriceForGivenBrand(arr, brandToSearch);
+        if (totalPrice > 0) {
+            System.out.println(totalPrice);
+        } else {
+            System.out.println("The given Brand is not available");
+        }
+
+        Phone phoneByOs = getPhoneIdBasedOnOs(arr, osToSearch);
+        if (phoneByOs != null) {
+            System.out.println(phoneByOs.getPhoneId());
+        } else {
+            System.out.println("No phones are available with specified os and price range");
+        }
+    }
+
+    public static int findPriceForGivenBrand(Phone[] arr, String brand) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getBrand().equalsIgnoreCase(brand)) {
+                sum += arr[i].getPrice();
+            }
+        }
+        return sum;
+    }
+
+    public static Phone getPhoneIdBasedOnOs(Phone[] arr, String os) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getOs().equalsIgnoreCase(os) && arr[i].getPrice() >= 50000) {
+                return arr[i];
+            }
+        }
+        return null;
+    }
+}
+
